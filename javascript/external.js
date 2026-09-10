099d979301c7c4cc35ea035dde68a2acbb22038f
jl1652 <jl1652@txstate.edu>
2026-09-09 20:12:12 -0500

    Completed Task 6 Array (Add Bracket to fix error)

--- javascript/external.js
+++ javascript/external.js
@@ -2,7 +2,7 @@ arr = ["Mael","Eric","Randall","James"];
 for (var i=0; i < arr.length; i++)
 {
 document.writeln(arr[i],"<br>");
-
+}
 
 //document.write("Where is my JS code?");
 //window.alert(5 + 6);
